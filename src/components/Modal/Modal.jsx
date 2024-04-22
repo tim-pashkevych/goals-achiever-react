e211c74ba27df31a14889616749be0458a10dbbf
BornBeBat <Nukkutos@gmail.com>
2024-04-22 14:51:08 +0300

    reusable modal

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,66 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { ModalButton, ModalWindow, Wrapper } from './Modal.styled';
+
+const modalRoot = document.querySelector('#rootModal');
+
+export const Modal = ({
+  children,
+  toggleModal,
+  previosModal = false,
+  padding = '24px',
+}) => {
+  useEffect(() => {
+    if (previosModal) return;
+    const handleEscape = (event) => {
+      if (event.code === 'Escape') {
+        toggleModal();
+      }
+    };
+    window.addEventListener('keydown', handleEscape);
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+      document.body.style.overflow = 'auto';
+    };
+  }, [toggleModal, previosModal]);
+
+  const handleClick = (event) => {
+    const { target, currentTarget } = event;
+    if (target === currentTarget) {
+      toggleModal();
+    }
+  };
+
+  return createPortal(
+    <Wrapper onClick={handleClick}>
+      <ModalWindow padding={padding}>
+        <ModalButton onClick={toggleModal}>
+          <svg
+            width="9"
+            height="9"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M10.5 1.5L1.5 10.5"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M1.5 1.5L10.5 10.5"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </ModalButton>
+        {children}
+      </ModalWindow>
+    </Wrapper>,
+    modalRoot
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export * from './Icon/Icon.jsx';
+export * from './Modal/Modal.jsx';
