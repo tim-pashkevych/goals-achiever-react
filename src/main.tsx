09190f41b3b329359f9ac8fd18a8f974c900fd19
BornBeBat <Nukkutos@gmail.com>
2024-04-23 16:45:41 +0300

    Them Provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, persistor } from './redux';
+import { store, persistor } from './redux/index.js';
+import { ThemeProvider } from './components/index.js';
 import App from './App.jsx';
 
 import 'modern-normalize/modern-normalize.css';
@@ -12,11 +13,13 @@ import './index.css';
 const { BASENAME = '/' } = import.meta.env;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={BASENAME}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>
+  <ThemeProvider>
+    <BrowserRouter basename={BASENAME}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </ThemeProvider>
 );
