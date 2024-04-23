ec6fdb53e3fbbe122cf745091a1e51c175c09fbe
BornBeBat <Nukkutos@gmail.com>
2024-04-23 23:27:10 +0300

    summary

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export const BOARD_BACKGROUNDS_COUNT = 15; //it means that you can choose index 
 export const DESKTOP_DEVICE = 'desktop';
 export const TABLET_DEVICE = 'tablet';
 export const MOBILE_DEVICE = 'mobile';
+export * from './themes.js';
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,22 +1,4 @@
 export const theme = Object.freeze({
-  dark: {
-    background: '#1F1F1F',
-    secondBackground: '#121212',
-    panelBackground: '#161616',
-    secondaryColor: '#BEDBB0',
-    textOne: '#FFFFFF',
-    textTwo: 'rgba(255, 255, 255, 0.8)',
-    textThree: 'rgba(255, 255, 255, 0.5)',
-    textFour: 'rgba(255, 255, 255, 0.1)',
-
-    themeSwitcher: 'rgba(255, 255, 255, 0.8)',
-    themeSwitsherAccent: '#bedbb0',
-    themeSwitsherPopUp: 'rgba(255, 255, 255, 0.5)',
-    themeSwitsherPopUpBorder: '#bedbb0',
-    themeSwitsherPopUpBackground: '#151515',
-    themeSwitsherPopUpBoxshadow: 'rgba(17, 17, 17, 0.1)',
-  },
-
   light: {
     background: '#f6f6f7',
     secondBackground: '#fff',
@@ -34,6 +16,24 @@ export const theme = Object.freeze({
     themeSwitsherPopUp: '#161616',
     themeSwitsherPopUpBorder: '#bedbb0',
     themeSwitsherPopUpBackground: '#fcfcfc',
+    themeSwitsherPopUpBoxshadow: 'rgba(17, 17, 17, 0.1)',
+  },
+
+  dark: {
+    background: '#1F1F1F',
+    secondBackground: '#121212',
+    panelBackground: '#161616',
+    secondaryColor: '#BEDBB0',
+    textOne: '#FFFFFF',
+    textTwo: 'rgba(255, 255, 255, 0.8)',
+    textThree: 'rgba(255, 255, 255, 0.5)',
+    textFour: 'rgba(255, 255, 255, 0.1)',
+
+    themeSwitcher: 'rgba(255, 255, 255, 0.8)',
+    themeSwitsherAccent: '#bedbb0',
+    themeSwitsherPopUp: 'rgba(255, 255, 255, 0.5)',
+    themeSwitsherPopUpBorder: '#bedbb0',
+    themeSwitsherPopUpBackground: '#151515',
     themeSwitsherPopUpBoxshadow: 'rgba(17, 17, 17, 0.1)',
   },
 
