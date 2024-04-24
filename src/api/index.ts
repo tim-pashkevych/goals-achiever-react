ed470b93d0e2586ea907c2a7d1693fb4d824a75c
Kovtunets Mykola <kovtunets73@gmail.com>
2024-04-24 10:46:47 +0200

    cards operations are done

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import users from './users/users';
+import cards from './cards/cards';
 //import your api endpoint group here
 
-export default { users }; //end then export it within this default object
+export default { users, cards }; //end then export it within this default object
 
 //this way you will be able to use api in your redux slice like :
 //import api from "src/api"
