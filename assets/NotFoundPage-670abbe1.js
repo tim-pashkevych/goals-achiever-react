6628adb2513a10b8a8fb45aabbb053bc7cae4124
tim-pashkevych <tymofii.pashkevych@gmail.com>
2024-04-24 02:44:48 +0000

    Deploying to gh-pages from @ tim-pashkevych/goals-achiever-react@fca79656e1345100ac593c2f3659ad10b995cf97 🚀

---
+++ assets/NotFoundPage-670abbe1.js
@@ -0,0 +1,20 @@
+import{u as t,j as e}from"./index-1db78e96.js";const n=t.div`
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`,i=t.a`
+  width: 335px;
+  color: white;
+  text-align: center;
+  padding: 14px 0;
+  border-radius: 8px;
+
+  background: rgb(22, 22, 22);
+
+  @media only screen and (min-width: 768px) {
+    width: 344px;
+  }
+`,r=()=>e.jsxs(n,{children:[e.jsx("h1",{children:"404"}),e.jsx("h2",{children:"Not found"}),e.jsx(i,{href:"/","aria-label":"Navigate to Home page",children:"Home page"})]});export{r as default};
