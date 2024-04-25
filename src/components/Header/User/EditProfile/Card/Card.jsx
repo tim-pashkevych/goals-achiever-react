62619d4e087ecf6c12f4bc23ea7deca8e251aac5
BornBeBat <Nukkutos@gmail.com>
2024-04-25 11:58:42 +0300

    preview avatar

--- src/components/Header/User/EditProfile/Card/Card.jsx
+++ src/components/Header/User/EditProfile/Card/Card.jsx
@@ -1,25 +1,73 @@
+import { useRef, useState } from 'react';
+import Resizer from 'react-image-file-resizer';
+
 import { Button } from '../../../../Button/Button';
 import { Icon } from '../../../../Icon/Icon';
 import {
   SButtonWraper_div,
+  SHideInput_input,
   SImageWrapper_div,
   SWrapper_div,
 } from './Card.styled';
 
+const resizeFile = (file) =>
+  new Promise((resolve) => {
+    Resizer.imageFileResizer(
+      file,
+      150,
+      150,
+      'JPEG',
+      100,
+      0,
+      (uri) => {
+        resolve(uri);
+      },
+      'file'
+    );
+  });
+
 export const Card = () => {
+  const [avatarUrl, setAvatarUrl] = useState(
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTousHbMHE9y6ZaMX9GcxFGJRfxq8aWGYcIV_dPgWOwqQ&s'
+  );
+  const inputFile = useRef(null);
+
+  const handleRedirectClick = () => {
+    inputFile.current.click();
+  };
+
+  const handleInputChange = async (e) => {
+    const reader = new FileReader();
+    const file = e.target.files[0];
+
+    if (!file) return;
+    const resizedFile = await resizeFile(file);
+    reader.onload = (e) => {
+      const resultSrc = e.target.result;
+      setAvatarUrl(resultSrc);
+    };
+
+    reader.readAsDataURL(resizedFile);
+  };
+
   return (
     <SWrapper_div>
       <SImageWrapper_div>
-        {true && (
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTousHbMHE9y6ZaMX9GcxFGJRfxq8aWGYcIV_dPgWOwqQ&s"
-            alt="sadasd"
-          />
-        )}
-        {false && <Icon id={'user'} size={48} />}
+        {avatarUrl && <img src={avatarUrl} alt="avatar" />}
+        {!avatarUrl && <Icon id={'user'} size={48} />}
       </SImageWrapper_div>
       <SButtonWraper_div>
-        <Button size="editProfile" type="button" />
+        <Button
+          size="editProfile"
+          type="button"
+          onClick={handleRedirectClick}
+        />
+        <SHideInput_input
+          onChange={handleInputChange}
+          ref={inputFile}
+          type="file"
+          accept="image/jpeg,image/png"
+        />
       </SButtonWraper_div>
     </SWrapper_div>
   );
--- src/components/Header/User/EditProfile/Card/Card.styled.jsx
+++ src/components/Header/User/EditProfile/Card/Card.styled.jsx
@@ -18,10 +18,28 @@ export const SImageWrapper_div = styled.div`
   stroke: ${({ theme }) => theme.userProfileIcon};
   fill: ${({ theme }) => theme.userProfileIcon};
   background-color: ${({ theme }) => theme.mainDashbordBackground};
+  & img {
+    height: 100%;
+    width: 100%;
+  }
 `;
 
 export const SButtonWraper_div = styled.div`
   position: absolute;
   bottom: 0;
   transform: translateY(50%);
+`;
+
+export const SHideInput_input = styled.input`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+
+  white-space: nowrap;
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
 `;
