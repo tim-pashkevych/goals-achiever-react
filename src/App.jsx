4397bb9ba0471df436bef65ed1e97294d2abf5fa
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-04-25 11:11:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import { GlobalStyle } from './styles/GlobalStyle.js';
 import { theme } from './styles/theme';
 import { Layout, ThemeContext } from './components';
 import { MainDashboard } from './components/MainDashboard/MainDashboard.jsx';
-// import { Sidebar } from './components/Sidebar/Sidebar.jsx';
 
 const WelcomePage = lazy(() => import('pages/WelcomePage/WelcomePage'));
 const AuthPage = lazy(() => import('pages/AuthPage/AuthPage.jsx'));
@@ -34,7 +33,6 @@ function App() {
           </Route>
           <Route path="/auth/dbord" element={<MainDashboard />} />
           <Route path="*" element={<NotFoundPage />} />
-          {/* <Sidebar /> */}
         </Route>
       </Routes>
     </ThemeProvider>
