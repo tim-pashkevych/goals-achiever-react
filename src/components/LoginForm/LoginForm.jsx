91db0ed3dcb96893872a30e3b20cdc20fa118a1e
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-04-25 15:57:00 +0300

    Login and Signup components: all logic moved from AuthPage to components

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
+import { useAppDispatch } from '../../hooks';
+import { loginThunk } from '../../redux';
+import { Button } from '..';
 import { SLogin_form, SLogin_input } from './LoginForm.styled';
 
-export const LoginForm = ({ handleChange }) => {
+export const LoginForm = () => {
+  const dispatch = useAppDispatch();
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const data = {
+      email: formData.email,
+      password: formData.password,
+    };
+    dispatch(loginThunk(data));
   };
   return (
     <div>
@@ -19,6 +37,7 @@ export const LoginForm = ({ handleChange }) => {
           placeholder="Create a password"
           onChange={handleChange}
         />
+        <Button title="Log In Now" icon={false} />
       </SLogin_form>
     </div>
   );
