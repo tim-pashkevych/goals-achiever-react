6e6cc3b1a85da2ff2854ec680fec4d874c0ede9e
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-04-26 18:20:42 +0300

    Added `stroke` and `fill` props to Icon component

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -3,9 +3,16 @@ import Icons from '../../assets/sprite.svg';
 
 // example:  <Icon id="plus" size={100} color="black" className="plus" />;
 
-export const Icon = ({ id, size, color, className }) => {
+export const Icon = ({ id, size, color, stroke, fill, className }) => {
   return (
-    <SIcon width={size} height={size} color={color} className={className}>
+    <SIcon
+      width={size}
+      height={size}
+      color={color}
+      stroke={stroke}
+      fill={fill}
+      className={className}
+    >
       <use href={Icons + '#icon-' + id}></use>
     </SIcon>
   );
