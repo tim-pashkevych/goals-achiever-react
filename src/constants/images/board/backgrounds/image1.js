5e9eacacc7edea19e3259fd58106f9aa75e5e073
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-04-26 18:17:59 +0300

    Board images: added icons and change a file structure.

---
+++ src/constants/images/board/backgrounds/image1.js
@@ -0,0 +1,37 @@
+import desktopImg1x from '../../../../assets/images/board/background/desktop/1-desktop.png';
+import desktopImg2x from '../../../../assets/images/board/background/desktop/1-desktop@2x.png';
+import desktopImgWebp1x from '../../../../assets/images/board/background/desktop/1-desktop.webp';
+import desktopImgWebp2x from '../../../../assets/images/board/background/desktop/1-desktop@2x.webp';
+import tabletImg1x from '../../../../assets/images/board/background/tablet/1-tablet.png';
+import tabletImg2x from '../../../../assets/images/board/background/tablet/1-tablet@2x.png';
+import tabletImgWebp1x from '../../../../assets/images/board/background/tablet/1-tablet.webp';
+import tabletImgWebp2x from '../../../../assets/images/board/background/tablet/1-tablet@2x.webp';
+import img1x from '../../../../assets/images/board/background/mobile/1-mobile.png';
+import img2x from '../../../../assets/images/board/background/mobile/1-mobile@2x.png';
+import imgWebp1x from '../../../../assets/images/board/background/mobile/1-mobile.webp';
+import imgWebp2x from '../../../../assets/images/board/background/mobile/1-mobile@2x.webp';
+
+const image = {
+  key: 'image1',
+  alt: 'Camping trailler at noght',
+  desktop: {
+    x1: desktopImg1x,
+    x2: desktopImg2x,
+    webp_x1: desktopImgWebp1x,
+    webp_x2: desktopImgWebp2x,
+  },
+  tablet: {
+    x1: tabletImg1x,
+    x2: tabletImg2x,
+    webp_x1: tabletImgWebp1x,
+    webp_x2: tabletImgWebp2x,
+  },
+  mobile: {
+    x1: img1x,
+    x2: img2x,
+    webp_x1: imgWebp1x,
+    webp_x2: imgWebp2x,
+  },
+};
+
+export default image;
---
+++ src/constants/images/board/backgrounds/index.js
@@ -0,0 +1,35 @@
+import image1 from './image1';
+import image2 from './image2';
+import image3 from './image3';
+import image4 from './image4';
+import image5 from './image5';
+import image6 from './image6';
+import image7 from './image7';
+import image8 from './image8';
+import image9 from './image9';
+import image10 from './image10';
+import image11 from './image11';
+import image12 from './image12';
+import image13 from './image13';
+import image14 from './image14';
+import image15 from './image15';
+import image16 from './image16';
+
+export const boardBackgoundImgs = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+];
