8b134f46ca585f4ec5e2ff0486742677484897ed
BornBeBat <Nukkutos@gmail.com>
2024-04-27 12:02:02 +0300

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const { VITE_BASE_URL } = import.meta.env;
 
 export const api = axios.create({
-  baseURL: 'https://goals-achiever.onrender.com/api/v1',
+  baseURL: 'http://localhost:3000/api/v1',
 });
 
 export const setAxiosToken = (token: string) => {
