aa6c3a1b9460a01a54b384ef137a99ec7a1f3f78
tim-pashkevych <tymofii.pashkevych@gmail.com>
2024-04-29 14:35:40 +0000

    Deploying to gh-pages from @ tim-pashkevych/goals-achiever-react@3fcd9a8ae4e6a68265e81f9f25ae72ce196717f4 🚀

--- assets/WelcomePage-9b327f65.js
+++ assets/WelcomePage-9b327f65.js
@@ -1,4 +1,4 @@
-import{u as e,L as n,a as r,s,j as t,I as l}from"./index-c9bc7b2d.js";import{I as d}from"./Image-36b7c753.js";import"./index-0e05dea9.js";const p=e.div`
+import{u as e,L as n,a as r,s,j as t,I as l}from"./index-2f30e0c3.js";import{I as d}from"./Image-d741a725.js";import"./index-0e05dea9.js";const p=e.div`
   height: 100vh;
   margin: 0 auto;
 
---
+++ assets/useModal-b4c18686.js
@@ -0,0 +1,1 @@
+import{r as s}from"./index-2f30e0c3.js";const r=()=>{const[o,t]=s.useState(!1);return[o,(e=!o)=>{t(e)}]};export{r as u};
