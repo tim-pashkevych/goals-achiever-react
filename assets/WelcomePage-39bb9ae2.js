079bff8b7592e8c9b353d9461d92c4cf1b66d462
tim-pashkevych <tymofii.pashkevych@gmail.com>
2024-04-30 08:38:37 +0000

    Deploying to gh-pages from @ tim-pashkevych/goals-achiever-react@d1c24d65be4cbffbcfe28b1de779d29146b38e72 🚀

--- assets/WelcomePage-39bb9ae2.js
+++ assets/WelcomePage-39bb9ae2.js
@@ -1,4 +1,4 @@
-import{u as e,L as n,a as r,s,j as t,I as l}from"./index-8866ba8e.js";import{I as d}from"./Image-54de2d14.js";import"./index-0e05dea9.js";const p=e.div`
+import{u as e,L as n,a as r,s,j as t,I as l}from"./index-99fc1da3.js";import{I as d}from"./Image-e53ca638.js";const p=e.div`
   height: 100vh;
   margin: 0 auto;
 
@@ -60,4 +60,4 @@ import{u as e,L as n,a as r,s,j as t,I as l}from"./index-8866ba8e.js";import{I a
   @media only screen and (min-width: 768px) {
     width: 344px;
   }
-`,m=()=>{const o=r(s),i={width:162,height:162},a={width:124,height:124};return!o&&t.jsxs(p,{children:[t.jsx(d,{desktop:i,tablet:i,mobile:a,type:"image/png",pathKey:"hero",alt:"Boy working on laptop"}),t.jsxs("h1",{children:[t.jsx(l,{id:"logo",size:48})," Task Pro"]}),t.jsx("p",{children:"Supercharge your productivity and take control of your tasks with Task Pro - Don't wait, start achieving your goals now!"}),t.jsxs("ul",{children:[t.jsx("li",{children:t.jsx(c,{to:"/auth/register","aria-label":"Link to register",children:"Registration"})}),t.jsx("li",{children:t.jsx(n,{to:"/auth/login","aria-label":"Link to login",children:"Log in"})})]})]})};export{m as default};
+`,x=()=>{const o=r(s),i={width:162,height:162},a={width:124,height:124};return!o&&t.jsxs(p,{children:[t.jsx(d,{desktop:i,tablet:i,mobile:a,type:"image/png",pathKey:"hero",alt:"Boy working on laptop"}),t.jsxs("h1",{children:[t.jsx(l,{id:"logo",size:48})," Task Pro"]}),t.jsx("p",{children:"Supercharge your productivity and take control of your tasks with Task Pro - Don't wait, start achieving your goals now!"}),t.jsxs("ul",{children:[t.jsx("li",{children:t.jsx(c,{to:"/auth/register","aria-label":"Link to register",children:"Registration"})}),t.jsx("li",{children:t.jsx(n,{to:"/auth/login","aria-label":"Link to login",children:"Log in"})})]})]})};export{x as default};
